2a8ca034c0aec86dff076916458af8fc1fd7b9c8
Dongsheng Cai <d@tux.im>
2014-08-02 01:22:42 +1000

    A new test file

---
+++ examples/test.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * @copyright  Dongsheng Cai {@see http://dongsheng.org}
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU Public License
+ */
+
+require_once('../curl.class.php');
+
+$curl = new curl();
+
+$r = $curl->get('http://google.com');
+
+echo $r;
+var_dump($r);
